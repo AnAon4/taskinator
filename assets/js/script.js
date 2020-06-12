c0248799303067db8b911faa36d85ad11e05166e
git config <michaelallenwright90@gmail.com>
2020-06-11 18:31:20 -0700

    create a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+tasksToDoEl;
 
-console.dir(window.document);
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "this is a new task";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
+
